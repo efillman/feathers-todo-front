de7c9f8329ecdf5f090d9f8970b4c953d6fe9834
Evan Fillman <efillman@gmail.com>
2021-08-24 11:41:50 -0400

    basis read of todos

--- src/App.js
+++ src/App.js
@@ -1,22 +1,60 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState, useEffect } from 'react';
+
+//http://localhost:3030/todos
+
+// {
+//   "total": 3,
+//   "limit": 10,
+//   "skip": 0,
+//   "data": [
+//       {
+//           "id": 1,
+//           "text": "Todo 1",
+//           "completed": false,
+//           "created_at": "2021-08-24T15:20:48.072Z",
+//           "updated_at": "2021-08-24T15:20:48.072Z"
+//       },
+//       {
+//           "id": 2,
+//           "text": "Todo 2",
+//           "completed": false,
+//           "created_at": "2021-08-24T15:20:48.072Z",
+//           "updated_at": "2021-08-24T15:20:48.072Z"
+//       },
+//       {
+//           "id": 3,
+//           "text": "Todo 3",
+//           "completed": false,
+//           "created_at": "2021-08-24T15:20:48.072Z",
+//           "updated_at": "2021-08-24T15:20:48.072Z"
+//       }
+//   ]
+// }
+
 
 function App() {
+  const [todos, setTodos] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  useEffect(() => {
+    setIsLoading(true);
+    fetch('http://localhost:3030/todos')
+      .then(response => response.json())
+      .then(data => setTodos(data))
+      .catch(err => console.log(err))
+    setIsLoading(false);
+  }, [])
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h3>Todo list</h3>
+        {isLoading ? <div>loading</div> :
+        <ul>
+          {todos.data.map(todo => (<li>{todo.text}</li>))}
+        </ul>
+        }
       </header>
     </div>
   );
